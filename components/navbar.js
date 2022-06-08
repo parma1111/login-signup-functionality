5ce424ada320564fad72ccc382f1d1149449393c
parma1111 <parmarambuldak800@gmail.com>
2022-06-08 05:42:38 -0700

    login-signup functionality

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+function navbar(){
+    return `
+    <div class="navbar">
+    <div id="home"><a href="index.html">Home</a></div>
+    <div id="Search_Bar"> 
+        <input type="text" placeholder="Search Recepie" id="Search_Recepie">
+    </div>
+    <div id="Latest_Recipes"><a href="latestRecipe.html">Latest Recipes</a></div>
+    <div id="Recipes_of_the_Day"><a href="RecipeOfDay.html">Recipes of the Day</a></div>
+    <div id="signup"><a href="signup.html">SignUp</a></div>
+    <div id="login"><a href="login.html">Login</a></div>
+
+</div>`
+}
+export default navbar;
